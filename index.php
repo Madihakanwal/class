474add8bceea0a0b2dc070eb32d08952835fc177
madiha <madihakanwal655@gmail.com>
2025-10-21 09:39:59 +0500

    php

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+ $number=212;
+ $name="Areeba";
+ $point_value=55.45;
+ $a=45;
+ $emojy="😂<br/>";
+ echo $emojy;
+ echo $a;
+ echo "<br/>";
+ echo $name;
+ echo "<br/>";
+
+ $country="pakistan";
+ echo "i love" .$country ;
+ echo " <br/>";
+ 
+ var_dump($name);
+ $boolean=true;
+ var_dump($boolean);
+
+ echo "<br>";
+
+$a=$b=$c=$d=$f=22;
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c;
+echo "<br>";
+echo $d;
+echo "<br>";
+echo $f;
+echo "<br>";
+
+echo "<h1>i am areeba </h1>";
+echo "<br>";
+
+print "hello";
+ echo "<br>";
+print "$name";
+ echo "<br>";
+
+ echo 'i love $name';
+  echo "<br>";
+ echo "i love $name";
+  echo "<br>";
+ $city=("#city");
+ echo $city;
+ echo "<br>";
+
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- this is a php file -->
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
